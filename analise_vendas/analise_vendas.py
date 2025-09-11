35ec5a6ec262601cb2e6eddd1e7344c9bd5a9107
Luizjr79 <luizjr79@outlook.com>
2025-09-11 01:09:44 -0300

    Update analise_vendas.py

--- analise_vendas/analise_vendas.py
+++ analise_vendas/analise_vendas.py
@@ -14,12 +14,17 @@ with open ("analise_vendas/vendas.csv","r",newline = "", encoding="UTF-8") as ar
 
   faturamento_total = 0
   faturamento_por_mes = {} # dicionário para guardar os valores por mês
+  quantidade_vendas = {}
 
   for linha in leitor: # Convertendo os valores numéricos de string -> int
     quantidade = int(linha["quantidade_vendida"])
     valor_unitario = float(linha["valor_unitario"])
     valor_total = float(linha["valor_total"])
     mes = linha["mes"]
+    produto = linha["produto"]
+
+    if produto not in quantidade_vendas:
+      quantidade_vendas[produto] = 0
 
     if mes not in faturamento_por_mes: # Se o mês ainda não está no dicionário, cria
       faturamento_por_mes[mes] = 0 
@@ -27,6 +32,7 @@ with open ("analise_vendas/vendas.csv","r",newline = "", encoding="UTF-8") as ar
     # Acumula o valor no mês correspondente
     faturamento_por_mes[mes] += valor_total
     faturamento_total += valor_total # Soma o valor total ao acumulador
+    quantidade_vendas[produto] += quantidade
 
     print(f"{linha['produto']} - {linha['mes']}: {quantidade} unidades, R$ {valor_total:.2f} ")
 
@@ -40,11 +46,15 @@ with open ("analise_vendas/vendas.csv","r",newline = "", encoding="UTF-8") as ar
 
   # Descobrir o mês campeão de vendas
   mes_campeao = max(faturamento_por_mes, key=faturamento_por_mes.get)
-  print(f"\n O mês de {mes_campeao} foi o campeão de faturamento, com R$ {faturamento_por_mes[mes_campeao]:.2f}")
+  print(f"\nO mês de {mes_campeao} foi o campeão de faturamento, com R$ {faturamento_por_mes[mes_campeao]:.2f}")
 
   # Descobrir o mês com menor faturamento
   mes_pior = min(faturamento_por_mes, key=faturamento_por_mes.get)
-  print(f"\n O mês de {mes_pior} apresentou o menor faturamento, com R$ {faturamento_por_mes[mes_pior]:.2f}")
+  print(f"\nO mês de {mes_pior} apresentou o menor faturamento, com R$ {faturamento_por_mes[mes_pior]:.2f}")
 
   print("\n===== FATURAMENTO SEMESTRE =====")
   print(f"O faturamento total do semestre é R$ {faturamento_total:.2f}")
+
+  # Descobrir o produto com maior quantidade de vendas  quantidade_vendida
+  produto_campeao = max(quantidade_vendas, key=quantidade_vendas.get)
+  print(f"\nO produto {produto_campeao} foi o mais vendido do semestre, com {quantidade_vendas[produto_campeao]} vendas")
